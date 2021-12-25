dc17a474d938aecab929abea836acb20499f60b0
ShakeebAftab <shakeebaftab@gmail.com>
2021-12-25 14:14:15 +0500

    Updated scrollbar and primary theme color

--- src/providers/Theme.tsx
+++ src/providers/Theme.tsx
@@ -1,5 +1,4 @@
 import { createTheme, ThemeProvider, CssBaseline } from "@material-ui/core";
-import { red } from "@material-ui/core/colors";
 import { createContext, Dispatch, FC, SetStateAction, useState } from "react";
 import { BrowserRouter } from "react-router-dom";
 
@@ -17,7 +16,27 @@ export const ThemeContextProvider: FC<any> = ({ children }) => {
     palette: {
       type: "dark",
       primary: {
-        main: red[600],
+        main: "#171717",
+      },
+      background: {
+        // default: "#111",
+      },
+    },
+    overrides: {
+      MuiCssBaseline: {
+        "@global": {
+          "*::-webkit-scrollbar": {
+            width: "0.4em",
+            height: "0.4em",
+          },
+          "*::-webkit-scrollbar-track": {
+            "-webkit-box-shadow": "inset 0 0 6px rgba(0,0,0,0.7)",
+          },
+          "*::-webkit-scrollbar-thumb": {
+            backgroundColor: "#707070",
+            borderRadius: 5,
+          },
+        },
       },
     },
   });
